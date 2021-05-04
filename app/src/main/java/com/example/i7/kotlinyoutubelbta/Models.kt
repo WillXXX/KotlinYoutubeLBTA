645e4c828dacccd3e941247fa23f01fb4540b8d6
Will <will.marcx@gmail.com>
2021-05-03 23:54:27 -0300

    *update libs

--- app/src/main/java/com/example/i7/kotlinyoutubelbta/Models.kt
+++ app/src/main/java/com/example/i7/kotlinyoutubelbta/Models.kt
@@ -2,7 +2,12 @@ package com.example.i7.kotlinyoutubelbta
 
 class HomeFeed(val videos: List<Videos>)
 
-class Videos(val id: Int, val name: String, val link: String, val imageUrl: String,
-             val numberOfViews: Int, val channel: Channel)
+class Videos(val id: Int,
+             val name: String,
+             val link: String,
+             val imageUrl: String,
+             val numberOfViews: Int,
+             val channel: Channel)
 
-class Channel(val name: String, val profileImageUrl: String)
+class Channel(val name: String,
+              val profileImageUrl: String)
